9a7581c5fc85c3e6b2d4dc821283117d8970f315
jainamjodhawat <144703962+jainamjodhawat@users.noreply.github.com>
2026-08-21 12:12:12 +0530

    feat: add recoverable production failure state

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import ProcurementCircuitBreaker from './ProcurementCircuitBreaker.tsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ProcurementCircuitBreaker>
+      <App />
+    </ProcurementCircuitBreaker>
   </React.StrictMode>
 );
